c7c132863439457dd1c9989a955ec380787747ff
Pablo Gancharov <pablogancharov@gmail.com>
2021-06-06 02:06:08 -0300

    updated

--- client/chunk.d6f17944.js
+++
@@ -1,2 +0,0 @@
-function e(){var e=[].slice.call(document.querySelectorAll("video.lazy"));if("IntersectionObserver"in window){var r=new IntersectionObserver(function(e,t){e.forEach(function(e){if(e.isIntersecting){for(var t in e.target.children){var a=e.target.children[t];"string"==typeof a.tagName&&"SOURCE"===a.tagName&&(a.src=a.dataset.src)}e.target.load(),e.target.classList.remove("lazy"),r.unobserve(e.target)}})});e.forEach(function(e){r.observe(e)})}}export{e as s};
-//# sourceMappingURL=chunk.d6f17944.js.map
---
+++ client/legacy/client.9d1ae12e.js
@@ -0,0 +1,1 @@
+import"./client.e5357a7e.js";
